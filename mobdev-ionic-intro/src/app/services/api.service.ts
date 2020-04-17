12fb08b24148599c0ee67e6b6b55e6db72fd144b
Joaocastron <joao.c.neto@hotmail.com>
2020-04-17 18:23:22 +0000

    Starting project

---
+++ mobdev-ionic-intro/src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+ 
+  constructor(private http: HttpClient) { }
+ 
+  getEpisodes() {
+    return this.http.get('https://breakingbadapi.com/api/episodes');
+  }
+ 
+  getEpisode(id) {
+    return this.http.get(`https://breakingbadapi.com/api/episode_id/${id}`);
+  }
+  getCharacters() {
+    return this.http.get('https://www.breakingbadapi.com/api/characters');
+  }
+ 
+  getCharacter(id) {
+    return this.http.get(`https://www.breakingbadapi.com/api/character/${id}`);
+  }
+}
